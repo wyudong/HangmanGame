17ccb3fffe968f26e748a30f81c376d8231beb9a
Wang Yudong <wangyudong.blue@163.com>
2015-08-18 21:45:50 +0800

    Initial Commit

---
+++ HangmanGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HangmanGame
+//
+//  Created by wyudong on 15/8/18.
+//  Copyright (c) 2015年 wyudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HangmanGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HangmanGame
+//
+//  Created by wyudong on 15/8/18.
+//  Copyright (c) 2015年 wyudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
